118eb306acf2f4b875648ccd477a9c8b661259e1
dev-keni <kennyjhwang@gmail.com>
2025-09-10 20:50:23 -0700

    lab3
    3

---
+++ GD13_1333_Lab3_Wang_Kenny/Lab3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GameManager
+
+            //Let each player pick which die to use
+            //Remove the chosen dice from the players inventory
+            //Calculate each players dice rolls (pick random number for each player according to dice), whichever player rolls the higher number gets 1 score
+            //Redo above 4 times
+            //If players are 2:2/tied, have them roll die again in sudden death
+        }
+    }
+}
